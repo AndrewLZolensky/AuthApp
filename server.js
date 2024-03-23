60bf2db5fc05229b59535c6f29ef0863365c1c94
Andrew Zolensky <andrewlzolensky@gmail.com>
2024-03-23 14:30:57 -0400

    added some server code

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const connectDB = require("./db")
+
+// create express app
 const app = express();
+
+// connect database
 connectDB();
+
+// open backend server on port 5000
 const PORT = 5000;
-app.listen(PORT, () => {
+server = app.listen(PORT, () => {
     console.log(`server connected to port ${PORT}`);
 })
+
+// handle uncaught promise rejections
