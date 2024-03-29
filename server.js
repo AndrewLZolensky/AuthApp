b5f4e0113f16c36e7e577a772dde4a49913dadae
Andrew Zolensky <andrewlzolensky@gmail.com>
2024-03-29 18:27:18 -0400

    added global exception handling in server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const connectDB = require("./db")
+const express = require("express"); // import express dependency
+const connectDB = require("./db"); // import function to connect to db
 
 // create express app
 const app = express();
 
-// connect database
+// connect database (async)
 connectDB();
 
 // open backend server on port 5000
@@ -14,3 +14,7 @@ server = app.listen(PORT, () => {
 })
 
 // handle uncaught promise rejections
+process.on("unhandledRejection", err => {
+    console.log(`An error occurred: ${err.message}`)
+    server.close(() => process.exit(1))
+  })
