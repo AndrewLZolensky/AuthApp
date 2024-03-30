1e16794f0f1202a17d4defa5a583bdc583b8836e
Andrew Zolensky <andrewlzolensky@gmail.com>
2024-03-29 23:06:01 -0400

    added login route to Auth Api at /auth/api defined in /Auth/Auth.js and /Auth/Route.js

--- Auth/Auth.js
+++ Auth/Auth.js
@@ -1,6 +1,7 @@
 // pull in user schema
 const User = require('../model/User.js');
 
+// define a route to register a new user
 exports.register = async (req, res, next) => {
     const { username, password } = req.body;
     if (password.length < 6) {
@@ -19,6 +20,36 @@ exports.register = async (req, res, next) => {
     } catch (err) {
         res.status(401).json({
             message: "User not created",
+            error: err.message
+        })
+    }
+}
+
+// define a route to log in
+exports.login = async (req, res, next) => {
+    const {username, password} = req.body;  // if username or password empty respond with error message
+    if (!username || !password) {
+        return res.status(400).json({
+            message: "Username or Password left blank"
+        })
+    }
+
+    try {
+        const user = await User.findOne({username, password});  // otherwise look up the user
+        if (!user) { // if user does not exsit respond with error message
+            res.status(401).json({
+                message: "Login not successful",
+                error: "User not found"
+            })
+        } else { // otherwise log in successful
+            res.status(200).json({
+                message: "Login successful",
+                user: user
+            })
+        }
+    } catch (err) { // catch error reading db
+        res.status(400).json({
+            message: "An error occurred",
             error: err.message
         })
     }
--- Auth/Route.js
+++ Auth/Route.js
@@ -5,10 +5,13 @@ const express = require('express');
 const router = express.Router();
 
 // pull in register function for registering new user
-const {register} = require("./Auth.js");
+const {register, login} = require("./Auth.js");
 
 // handle <POST request to "/register"> with the register() function
 router.route("/register").post(register);
+
+// handle <POST to "/login"> with login() function
+router.route("/login").post(login);
 
 // make this router available in other js files (i.e. server.js)
 module.exports = router;
